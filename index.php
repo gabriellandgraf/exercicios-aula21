3d86dc92065c2351709d0fe3bc94ddbd619b1027
gabriellandgraf <gabriellandgrafdecarvalho54@gmail.com>
2024-07-24 17:18:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/*$nota1=50;
+$nota2=20;
+
+$soma= $nota1 + $nota2;
+
+$mult= ($soma*$nota1);
+
+echo $mult;
+
+
+/valor_um= 45;
+$valor_dois= 55;
+$valor_tres= 75;
+
+$media= ($valor_um + $valor_dois + $valor_tres)/3;
+
+echo "$media";*/
+
+//3 – Escreva um algoritmo que receba um valor, calcule e mostre parausuário 15% deste valor.
+
+$a= 12;
+$b=15;
+$calc= ($a+$b) /15;
+
+echo "$calc <br/>\n";
